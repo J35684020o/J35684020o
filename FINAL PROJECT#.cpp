1e76c39ffb552901476f684964fcbd849ef70c5b
J35684020o <85627644+J35684020o@users.noreply.github.com>
2021-08-17 15:24:32 +0300

    Add files via upload

---
+++ FINAL PROJECT#.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<windows.h>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{  int i,First_Lesson,Second_Lesson,Third_Lesson,Fourth_Lesson,Fifth_Lesson,Sixth_Lesson,Seventh_Lesson,Eighth_Lesson;
+   int First_Break,Second_Break,Lunch,Games;
+	
+	for(i=0;i<1;i++)
+	{
+	First_Lesson;
+	 Beep(2200,5000);
+	 
+	sleep(8);
+	
+	Second_Lesson;
+     Beep(2200,5000);
+     
+     sleep(8);
+	
+	First_Break;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	
+	Third_Lesson;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	 
+	Fourth_Lesson;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	 
+	Second_Break;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	 
+	Fifth_Lesson;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	 
+	Sixth_Lesson;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	 
+	Lunch;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	 
+	Seventh_Lesson;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	 
+	Eighth_Lesson;
+	 Beep(2200,5000);
+	 
+	 sleep(8);
+	 
+	Games;
+	 Beep(2200,5000);
+	 
+	 
+	 
+    }
+	
+	return 0;
+}
